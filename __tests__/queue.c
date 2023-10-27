2400a020a6cd57e29ad7c1f874f4827d1f8440ea
el-damiano <el9damiano@gmail.com>
2023-10-27 18:15:06 +0200

    test: Added simple tests for queue

---
+++ __tests__/queue.c
@@ -0,0 +1,35 @@
+#include "../queue.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+unsigned long glength = 0;
+
+struct Node *phead = NULL;
+struct Node *ptail = NULL;
+
+int main() {
+    // Caveman tests (to refactor one day)
+    int counter = 0;
+
+    // Test for empty
+    if (peek(&phead) == -1) counter++;
+
+    // Enqueue and test if head is correct
+    enqueue(&phead, &ptail, 10);
+    enqueue(&phead, &ptail, 0);
+    enqueue(&phead, &ptail, 6);
+    if (peek(&phead) == 10) counter++;
+
+    // Dequeque and test if head is correct
+    dequeue(&phead, &ptail);
+    if (peek(&phead) == 0) counter++;
+
+    dequeue(&phead, &ptail);
+    if (peek(&phead) == 6) counter++;
+
+    // Dequeque last item and test if empty
+    dequeue(&phead, &ptail);
+    if (peek(&phead) == -1) counter++;
+
+    printf("%d out of %d tests have finished successfully\n", counter, 5);
+}
